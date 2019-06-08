bb09871f42cea1e94d2571b9cc56890114bfb753
Engr.Casmir <casmir@localhost.localdomain>
2019-06-08 12:01:25 -0400

    adding first commit in local repo

---
+++ group/eec.js
@@ -0,0 +1,40 @@
+(function(){
+    function gruopMembers(){
+    const members  =prompt('what is your REG NO ? ')
+}
+let casmir= gruopMembers();
+
+let chimaobi=gruopMembers();
+
+let n1=gruopMembers();
+
+let n2=gruopMembers();
+
+let n3=gruopMembers();
+
+let n4=gruopMembers();
+
+  //  let member= member.toLowerCase()
+
+    if (members.includes(109)){alert('Onyiba Casmir is a member of group') }
+   
+    else if(members.includes(109)){alert('Onyiba Casmir is a member of group')}
+
+    else if(members.includes(109)){alert('Onyiba Casmir is a member of group')}
+
+    else if(members.includes(115)){alert('Caleb is a member of the group')}
+
+    else if(members.includes(108)){alert('i think Anele Chimaobi is a gruop member')}
+
+    else if(members.includes(108)){alert('i think Anele Chimaobi is a gruop member')}
+
+    else if(members.includes(108)){alert('i think Anele Chimaobi is a gruop member')}
+
+    else if(members.includes(108)){alert('i think Anele Chimaobi is a gruop member')}
+
+    else if(members.includes(108 || 109 || 154 || 103 ||186 || 195||106 ||107)){alert('these are people are group members')}
+
+else{alert(`this person is not among the group`)}
+
+
+})()
